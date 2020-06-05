c50fc510ca0e91602612d6ebe7716f63d5b87d43
wxqSnowing <707409166@qq.com>
2020-06-05 19:07:12 +0800

    update

--- dao/comment/comment_sql.js
+++ dao/comment/comment_sql.js
@@ -1,6 +1,6 @@
 // 对数据库中的comment表进行增，删，查操作语句
 
 module.exports = {
-    queryWorkCommentSQL: "SELECT * FROM work_comment_info where workid = ?;",
+    queryWorkCommentSQL: "SELECT * FROM work_comment_info where workid = ? order by createtime desc;",
     addWorkCommentSQL: "insert into work_comment_info (workid, uid, comment) value (?, ?, ?);",
 }
