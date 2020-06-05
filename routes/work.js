5ba96e588b4575ad7dc1508bcc2be91b8f934924
wxqSnowing <707409166@qq.com>
2020-06-05 11:17:46 +0800

    update

--- routes/work.js
+++ routes/work.js
@@ -189,38 +189,4 @@ router.post('/api/add_work', function(req, res, next) {
     })
 });
 
-
-//通过oss上传封面图，其中遇到的问题是:上传后打开链接是下载，需要设置headers，client.put方法调用时使用
-let OSS = require('ali-oss');
-let client = new OSS({
-    region: 'xxx',
-    accessKeyId: 'xxx',
-    accessKeySecret: 'xxx',
-    bucket: 'image-bucket-6',
-});
-
-async function uploadWorkImage(imagePath, callback) {
-    try {
-        const headers = {
-                'Content-Type': 'image/jpg',
-            }
-            //object-name可以自定义为文件名（例如file.txt）或目录（例如abc/test/file.txt）的形式，实现将文件上传至当前Bucket或Bucket下的指定目录。
-        let newName = '/images/' + parseInt((Math.random() * 10000)) + '.jpg';
-        console.log(newName, '@@@@####');
-
-        let result = await client.put(newName, imagePath, { headers });
-        callback(result);
-    } catch (e) {
-        console.log(e);
-    }
-}
-//上传封面图
-router.put('/api/add_work_image', function(req, res, next) {
-    let imagePath = req.query.imagePath;
-    uploadWorkImage(imagePath, function(success) {
-        res.json(success);
-    })
-});
-
-
 module.exports = router;
